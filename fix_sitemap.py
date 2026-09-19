782bed7ad6db353b4218d05d34529c126c3ecc73
Monash Calculator <monashwamcalculator@gmail.com>
2026-09-19 17:24:46 +0530

    Remove utility scripts

--- fix_sitemap.py
+++
@@ -1,7 +0,0 @@
-import os, re
-sitemap_path = 'sitemap.xml'
-with open(sitemap_path, 'r', encoding='utf-8') as f:
-    sitemap = f.read()
-new_sitemap = re.sub(r'<url>\s*<loc>[^<]*(/listings/)[^<]*</loc>.*?</url>', '', sitemap, flags=re.IGNORECASE|re.DOTALL)
-with open(sitemap_path, 'w', encoding='utf-8') as f:
-    f.write(new_sitemap)
